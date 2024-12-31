2589d615302852bcf472a61acc0e596a55c000d6
bgomez-2022208 <bgomez-2022208@kinal.edu.gt>
2024-12-30 23:43:36 -0600

    cambios a los archivos para mejorar la estructura

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,52 @@
-const Cart = () => {
-    return <div>Cart Page</div>;
-};
+import { useState, useEffect } from 'react';
+import { AddToCartIcon } from "./Home/components/icons.jsx";
+import "./Products.css";
+import { getProducts } from "../services/productService.js";
 
-export default Cart;
+export default function Cart() {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const data = await getProducts();
+                setProducts(data);
+            } catch (error) {
+                console.error('Error al obtener los productos', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
+    if (loading) {
+        return <div>Cargando productos...</div>;
+    }
+
+    if (products.length === 0) {
+        return <div>No se encontraron productos.</div>;
+    }
+
+    return (
+        <main className="products">
+            <ul>
+                {products.map((product) => (
+                    <li key={product.idProductos}>
+                        <img src={`data:image/jpeg;base64,${product.fotoProducto}`} alt={product.nombreProducto} />
+                        <div>
+                            <strong>{product.nombreProducto}</strong> - ${product.precioProducto}   -   stock:{product.stock}
+                        </div>
+                        <div>
+                            <button>
+                                <AddToCartIcon />
+                            </button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
+}
--- src/pages/Home/ClientHome.jsx
+++ src/pages/Home/ClientHome.jsx
@@ -1,52 +1,10 @@
-import { useState, useEffect } from 'react';
-import { AddToCartIcon } from "./components/icons.jsx";
-import "./Products.css";
-import { getProducts } from "../../services/productService.js";
+import Cart from "../Cart.jsx";
 
 export default function ClientHome() {
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                const data = await getProducts();
-                setProducts(data);
-            } catch (error) {
-                console.error('Error al obtener los productos', error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchProducts();
-    }, []);
-
-    if (loading) {
-        return <div>Cargando productos...</div>;
-    }
-
-    if (products.length === 0) {
-        return <div>No se encontraron productos.</div>;
-    }
 
     return (
-        <main className="products">
-            <ul>
-                {products.map((product) => (
-                    <li key={product.idProductos}>
-                        <img src={`data:image/jpeg;base64,${product.fotoProducto}`} alt={product.nombreProducto} />
-                        <div>
-                            <strong>{product.nombreProducto}</strong> - ${product.precioProducto}   -   stock:{product.stock}
-                        </div>
-                        <div>
-                            <button>
-                                <AddToCartIcon />
-                            </button>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </main>
+        <Cart />
     );
+
 }
